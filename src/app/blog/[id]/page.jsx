8c6e257b59fcd893eeb72fe50173e9fb844d932e
3 Moon <3moon@3s-MacBook-Pro-2.local>
2023-07-18 11:23:14 -0400

    seventh commit

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,9 +4,11 @@ import Image from 'next/image'
 import { notFound } from "next/navigation"
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  // const res = await fetch("http://localhost:3000/api/posts/${id}", {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts/${id}`, {
      cache: 'no-store',
      })
+     
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
  
