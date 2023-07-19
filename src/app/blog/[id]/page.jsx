876d2e0794847b205b41e0de09641d1042c07407
3 Moon <3moon@3s-MacBook-Pro-2.local>
2023-07-18 20:45:01 -0400

    21

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,8 @@ import Image from 'next/image'
 import { notFound } from "next/navigation"
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+     const res = await fetch(`https://ocanza-prototype.vercel.app/api/posts/${id}`, {
+  // const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     // const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/posts/${id}`, {
      cache: 'no-store',
      })
